d83ee682ec8582c17fcb89ab41913a7927378987
Jeff <jeffjens11@gmail.com>
2019-03-20 18:12:49 -0700

    Added profile update ability

--- ChoreApp.API/Helpers/AutoMapperProfiles.cs
+++ ChoreApp.API/Helpers/AutoMapperProfiles.cs
@@ -24,6 +24,7 @@ namespace ChoreApp.API.Helpers
                 });
                 
             CreateMap<Photo, PhotosForDetailDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
