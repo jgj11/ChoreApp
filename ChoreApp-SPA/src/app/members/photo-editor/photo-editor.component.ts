e9360a3f83bb06363165e53cbd1d1fb92e31772c
Jeff <jeffjens11@gmail.com>
2019-03-22 18:21:37 -0700

    Finished with Reactive Forms

--- ChoreApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ ChoreApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -51,6 +51,11 @@ export class PhotoEditorComponent implements OnInit {
           isMain: res.isMain
         };
         this.photos.push(photo);
+        if (photo.isMain) {
+          this.authService.changeMemberPhoto(photo.url);
+          this.authService.currentUser.photoUrl = photo.url;
+          localStorage.setItem('user', JSON.stringify(this.authService.currentUser));
+        }
       }
     };
   }
@@ -61,6 +66,8 @@ export class PhotoEditorComponent implements OnInit {
       this.currentMain.isMain = false;
       photo.isMain = true;
       this.authService.changeMemberPhoto(photo.url);
+      this.authService.currentUser.photoUrl = photo.url;
+      localStorage.setItem('user', JSON.stringify(this.authService.currentUser));
     }, error => {
       this.alertify.error(error);
     });
